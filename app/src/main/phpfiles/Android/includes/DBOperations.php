0de5c6c49a8f1386456e467de571535a115b8c64
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-08-17 11:52:35 +0530

    added a script to log in the user after he has registered

--- app/src/main/phpfiles/Android/includes/DBOperations.php
+++ app/src/main/phpfiles/Android/includes/DBOperations.php
@@ -29,6 +29,24 @@
 			}
 		}
 
+		public function userLogin($username, $pass)
+		{
+			$password = md5($pass);
+			$stmt = $this->con->prepare("SELECT id FROM users WHERE username = ? AND password = ?");
+			$stmt->bind_param("ss",$username,$password);
+			$stmt->execute();
+			$stmt->store_result();
+			return $stmt->num_rows > 0;
+		}
+
+		public function getUserByUsername($username)
+		{
+			$stmt = $this->con->prepare("SELECT * FROM users WHERE username = ?");
+			$stmt->bind_param("s",$username);
+			$stmt->execute();
+			return $stmt->get_result()->fetch_assoc();
+		}
+
 		private function isUserExist($username, $email){
 			$stmt = $this->con->prepare("SELECT id FROM users WHERE username = ? OR email = ?");
 			$stmt->bind_param("ss",$username,$email);
