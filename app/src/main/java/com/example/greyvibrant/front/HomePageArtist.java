1b24418d760306a8f178ab0948728f5ea48f5fdf
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-10-18 13:06:34 +0530

    added drawer layouts to both the home pages

--- app/src/main/java/com/example/greyvibrant/front/HomePageArtist.java
+++ app/src/main/java/com/example/greyvibrant/front/HomePageArtist.java
@@ -1,16 +1,22 @@
 package com.example.greyvibrant.front;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.example.greyvibrant.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomePageArtist extends AppCompatActivity {
+    private DrawerLayout artist_home_page_dl;
+    private ActionBarDrawerToggle abdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,28 @@ public class HomePageArtist extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_artist);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.artist_fragment_container, new AlbumFragment()).commit();
+        artist_home_page_dl = findViewById(R.id.artist_home_page_dl);
+        abdt = new ActionBarDrawerToggle(this, artist_home_page_dl, R.string.Open, R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+        artist_home_page_dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        final NavigationView nav_view = findViewById(R.id.artist_home_page_navigation_view);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                int id = menuItem.getItemId();
+//                if (id == R.id.profile_page) {
+//                    Toast.makeText(HomePageUser.this, "My profile", Toast.LENGTH_SHORT).show();
+//                } else if (id == R.id.logout) {
+//                    Toast.makeText(HomePageUser.this, "Log out", Toast.LENGTH_SHORT).show();
+//                }
+                return true;
+            }
+        });
     }
+
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
@@ -37,4 +64,23 @@ public class HomePageArtist extends AppCompatActivity {
                     return true;
                 }
             };
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (abdt.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void ProfilePage(MenuItem item) {
+        Intent intent = new Intent(HomePageArtist.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void Logout(MenuItem item) {
+        Intent intent = new Intent(HomePageArtist.this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/greyvibrant/front/HomePageUser.java
+++ app/src/main/java/com/example/greyvibrant/front/HomePageUser.java
@@ -1,16 +1,23 @@
 package com.example.greyvibrant.front;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.greyvibrant.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomePageUser extends AppCompatActivity {
+    private DrawerLayout user_home_page_dl;
+    private ActionBarDrawerToggle abdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,26 @@ public class HomePageUser extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_user);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.user_fragment_container, new HomeFragment()).commit();
+        user_home_page_dl = findViewById(R.id.user_home_page_dl);
+        abdt = new ActionBarDrawerToggle(this, user_home_page_dl, R.string.Open, R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+        user_home_page_dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        final NavigationView nav_view = findViewById(R.id.user_home_page_navigation_view);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                int id = menuItem.getItemId();
+//                if (id == R.id.profile_page) {
+//                    Toast.makeText(HomePageUser.this, "My profile", Toast.LENGTH_SHORT).show();
+//                } else if (id == R.id.logout) {
+//                    Toast.makeText(HomePageUser.this, "Log out", Toast.LENGTH_SHORT).show();
+//                }
+                return true;
+            }
+        });
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
@@ -42,4 +69,23 @@ public class HomePageUser extends AppCompatActivity {
                     return true;
                 }
             };
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (abdt.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void ProfilePage(MenuItem item) {
+        Intent intent = new Intent(HomePageUser.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void Logout(MenuItem item) {
+        Intent intent = new Intent(HomePageUser.this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
