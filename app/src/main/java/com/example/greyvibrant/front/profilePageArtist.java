61867b3d15f97eb09cdc5b9b1ea269e42cb2a022
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-10-20 15:14:48 +0530

    profile pages added

---
+++ app/src/main/java/com/example/greyvibrant/front/profilePageArtist.java
@@ -0,0 +1,16 @@
+package com.example.greyvibrant.front;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.greyvibrant.R;
+
+public class profilePageArtist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page_artist);
+    }
+}
