aa49323cc60b1cb65b964783d2f82bcf4f439bf6
Sabios-97 <sayan.biswas089@gmail.com>
2019-10-21 14:55:44 +0530

    user and artist profile backend

---
+++ app/src/main/phpfiles/artist_profile.php
@@ -0,0 +1,47 @@
+<?php
+$conn = mysqli_connect("localhost", "id11221849_sabios", "Sayan@99", "id11221849_greyvibrant");
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $artistname = $_POST['artistname'];
+    
+
+    $sql = "SELECT * FROM artist_registration WHERE artistname='$artistname'";
+
+    $response = mysqli_query($conn, $sql);
+
+    $result = array();
+    $result['profile'] = array();
+
+    if (mysqli_num_rows($response) === 1) {
+
+        $row = mysqli_fetch_assoc($response);
+
+        
+
+            $index['AID'] = $row['AID'];
+            $index['artistname'] = $row['artistname'];
+            $index['fullname'] = $row['fullname'];
+            $index['email'] = $row['email'];
+            $index['phNo'] = $row['phNo'];
+
+
+            array_push($result['profile'], $index);
+
+            $result['success'] = "1";
+            $result['message'] = "success";
+            echo json_encode($result);
+
+            mysqli_close($conn);
+
+         
+         }else {
+
+            $result['success'] = "0";
+            $result['message'] = "error";
+            echo json_encode($result);
+
+            mysqli_close($conn);
+        }
+    
+}
+?>
