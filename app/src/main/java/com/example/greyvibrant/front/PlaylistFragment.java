a66fef7ec2e579fbd429acb7830fdca49ddab834
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-10-27 16:34:27 +0530

    added adapters

--- app/src/main/java/com/example/greyvibrant/front/PlaylistFragment.java
+++ app/src/main/java/com/example/greyvibrant/front/PlaylistFragment.java
@@ -1,5 +1,7 @@
 package com.example.greyvibrant.front;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,15 +10,38 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.greyvibrant.R;
+import com.example.greyvibrant.front.RecyclerViewElements.playlistItem;
+import com.example.greyvibrant.front.adapter.PlaylistFragmentAdapter;
+
+import java.util.ArrayList;
 
 public class PlaylistFragment extends Fragment {
+
+    SharedPreferences sharedPreferences;
+    private RecyclerView mRecyclerViewPlaylist;
+    private PlaylistFragmentAdapter mPlaylistAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    ArrayList<playlistItem> playList = new ArrayList<>();
+    String songname, genre, album, language, UIDput;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.user_fragment_playlist, container, false);
-
+        View view = inflater.inflate(R.layout.user_fragment_playlist, container, false);
+        sharedPreferences = getContext().getSharedPreferences("com.example.greyvibrant.front", Context.MODE_PRIVATE);
+        UIDput = sharedPreferences.getString("UID", null);
+        /* song_retrieval */
+        mRecyclerViewPlaylist.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mPlaylistAdapter = new PlaylistFragmentAdapter(playList);
+        mRecyclerViewPlaylist.setLayoutManager(mLayoutManager);
+        mRecyclerViewPlaylist.setAdapter(mPlaylistAdapter);
+        mPlaylistAdapter.notifyDataSetChanged();
+        mRecyclerViewPlaylist = view.findViewById(R.id.recycler_view_playlist);
+        return view;
     }
 }
--- app/src/main/java/com/example/greyvibrant/front/QueueFragment.java
+++ app/src/main/java/com/example/greyvibrant/front/QueueFragment.java
@@ -1,5 +1,7 @@
 package com.example.greyvibrant.front;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,15 +10,38 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.greyvibrant.R;
+import com.example.greyvibrant.front.RecyclerViewElements.queueItem;
+import com.example.greyvibrant.front.adapter.QueueFragmentAdapter;
+
+import java.util.ArrayList;
 
 public class QueueFragment extends Fragment {
+
+    SharedPreferences sharedPreferences;
+    private RecyclerView mRecyclerViewQueue;
+    private QueueFragmentAdapter mQueueAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    ArrayList<queueItem> queueList = new ArrayList<>();
+    String songname, genre, album, language, UIDput;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.user_fragment_queue, container, false);
-
+        View view = inflater.inflate(R.layout.user_fragment_queue, container, false);
+        sharedPreferences = getContext().getSharedPreferences("com.example.greyvibrant.front", Context.MODE_PRIVATE);
+        UIDput = sharedPreferences.getString("UID", null);
+        /* song_retrieval */
+        mRecyclerViewQueue.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mQueueAdapter = new QueueFragmentAdapter(queueList);
+        mRecyclerViewQueue.setLayoutManager(mLayoutManager);
+        mRecyclerViewQueue.setAdapter(mQueueAdapter);
+        mQueueAdapter.notifyDataSetChanged();
+        mRecyclerViewQueue = view.findViewById(R.id.recycler_view_queue);
+        return view;
     }
 }
