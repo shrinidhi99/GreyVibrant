58a1f606180326247b00e429f1bd46875eebd55a
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-11-02 17:59:44 +0530

    added php files

---
+++ app/src/main/phpfiles/delete_artist.php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_connect("localhost", "id11221849_sabios", "Sayan@99", "id11221849_greyvibrant");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $AID = $_POST['AID'];
+
+    $sql = "DELETE FROM artist_registration WHERE AID='$AID'";
+
+    if (mysqli_query($conn, $sql)) {
+
+        $result["success"] = "1";
+        $result["message"] = "success";
+
+        echo json_encode($result);
+        mysqli_close($conn);
+    } else {
+
+        $result["success"] = "0";
+        $result["message"] = "Error";
+
+        echo json_encode($result);
+        mysqli_close($conn);
+    }
+}
---
+++ app/src/main/phpfiles/playlist_delete.php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_connect("localhost", "id11221849_sabios", "Sayan@99", "id11221849_greyvibrant");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $SID = $_POST['SID'];
+
+    $sql = "DELETE FROM playlist_name WHERE SID='$SID'";
+
+    if (mysqli_query($conn, $sql)) {
+
+        $result["success"] = "1";
+        $result["message"] = "success";
+
+        echo json_encode($result);
+        mysqli_close($conn);
+    } else {
+
+        $result["success"] = "0";
+        $result["message"] = "Error";
+
+        echo json_encode($result);
+        mysqli_close($conn);
+    }
+}
