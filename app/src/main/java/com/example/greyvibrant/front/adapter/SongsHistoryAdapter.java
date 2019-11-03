72fca33f91d985b28414e53caaa3ebf70bc0c422
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-11-03 11:08:06 +0530

    songs history  adapter

--- app/src/main/java/com/example/greyvibrant/front/adapter/SongsHistoryAdapter.java
+++ app/src/main/java/com/example/greyvibrant/front/adapter/SongsHistoryAdapter.java
@@ -1,8 +1,6 @@
 package com.example.greyvibrant.front.adapter;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.util.Log;
+import android.content.Context;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,7 +11,6 @@ import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.greyvibrant.R;
@@ -22,15 +19,91 @@ import com.example.greyvibrant.front.RecyclerViewElements.SongsHistoryItem;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SongsHistoryAdapter extends RecyclerView.Adapter<SongsHistoryAdapter.SongsHistoryViewHolder> implements Filterable, View.OnClickListener {
 
+public class SongsHistoryAdapter extends RecyclerView.Adapter<SongsHistoryAdapter.SongsHistoryViewHolder> implements Filterable {
+    private Context mContext;
     static List<SongsHistoryItem> mSongsHistoryList;
     private List<SongsHistoryItem> songsHistoryListFull;
     private OnItemClickListener mListener;
 
+    public SongsHistoryAdapter(Context context, List<SongsHistoryItem> exampleList) {
+        mContext = context;
+        mSongsHistoryList = exampleList;
+        songsHistoryListFull = new ArrayList<>(exampleList);
+    }
 
     @Override
-    public void onClick(View view) {
+    public SongsHistoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.songshistoryitem, parent, false);
+        return new SongsHistoryViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(SongsHistoryViewHolder holder, int position) {
+        SongsHistoryItem currentItem = mSongsHistoryList.get(position);
+        holder.mSongname.setText(currentItem.getmSongname());
+        holder.mArtistname.setText(currentItem.getmArtistname());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSongsHistoryList.size();
+    }
+
+    public class SongsHistoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnCreateContextMenuListener, MenuItem.OnMenuItemClickListener {
+        private TextView mSongname;
+        private TextView mArtistname;
+
+        public SongsHistoryViewHolder(View itemView) {
+            super(itemView);
+            mSongname = itemView.findViewById(R.id.song_name_songs_history);
+            mArtistname = itemView.findViewById(R.id.song_artist_songs_history);
+            itemView.setOnClickListener(this);
+            itemView.setOnCreateContextMenuListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mListener != null) {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    mListener.onItemClick(position);
+                }
+            }
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+            menu.setHeaderTitle("Select Action");
+            MenuItem playSong = menu.add(Menu.NONE, 1, 1, "Add to queue");
+            MenuItem deleteFromHistory = menu.add(Menu.NONE, 2, 2, "Remove from history");
+            MenuItem showDescription = menu.add(Menu.NONE, 3, 3, "More information");
+            playSong.setOnMenuItemClickListener(this);
+            deleteFromHistory.setOnMenuItemClickListener(this);
+            showDescription.setOnMenuItemClickListener(this);
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            if (mListener != null) {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+
+                    switch (item.getItemId()) {
+                        case 1:
+                            mListener.onPlayClick(position);
+                            return true;
+                        case 2:
+                            mListener.onDeleteFromHistoryClick(position);
+                            return true;
+                        case 3:
+                            mListener.onDescriptionClick(position);
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
 
     }
 
@@ -44,111 +117,8 @@ public class SongsHistoryAdapter extends RecyclerView.Adapter<SongsHistoryAdapte
         void onDescriptionClick(int position);
     }
 
-
     public void setOnItemClickListener(OnItemClickListener listener) {
         mListener = listener;
-    }
-
-    class SongsHistoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnCreateContextMenuListener, MenuItem.OnMenuItemClickListener {
-        private TextView mSongname;
-        private TextView mArtistname;
-
-
-        SongsHistoryViewHolder(@NonNull final View itemView) {
-            super(itemView);
-            mSongname = itemView.findViewById(R.id.song_name_songs_history);
-            mArtistname = itemView.findViewById(R.id.song_artist_songs_history);
-            itemView.setOnClickListener(this);
-            itemView.setOnCreateContextMenuListener(this);
-        }
-
-        @Override
-        public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
-            contextMenu.setHeaderTitle("Select Action");
-            MenuItem playSong = contextMenu.add(Menu.NONE, 1, 1, "Add to queue");
-            MenuItem deleteFromHistory = contextMenu.add(Menu.NONE, 2, 2, "Remove from history");
-            MenuItem showDescription = contextMenu.add(Menu.NONE, 3, 3, "More information");
-            playSong.setOnMenuItemClickListener(this);
-            deleteFromHistory.setOnMenuItemClickListener(this);
-            showDescription.setOnMenuItemClickListener(this);
-        }
-
-        @Override
-        public boolean onMenuItemClick(MenuItem menuItem) {
-            if (mListener != null) {
-                final int position = getAdapterPosition();
-                if (position != RecyclerView.NO_POSITION) {
-                    final SongsHistoryItem clickedItem3 = mSongsHistoryList.get(position);
-                    switch (menuItem.getItemId()) {
-                        case 1:
-                            mListener.onPlayClick(position);
-                            Log.d("on item click remaining", "onPlayClick at position: " + position);
-                            return true;
-                        case 2:
-                            mListener.onDeleteFromHistoryClick(position);
-                            Log.d("on item click remaining", "onDeleteFromItemClick at position: " + position);
-                            return true;
-                        case 3:
-                            mListener.onDescriptionClick(position);
-                            Log.d("on item click remaining", "onDescriptionClick at position: " + position);
-
-                            AlertDialog.Builder builder = new AlertDialog.Builder(itemView.getContext());
-                            builder.setTitle("More Info")
-                                    .setIcon(R.drawable.ic_audiotrack_black_24dp)
-                                    .setMessage("Song name:   " + clickedItem3.getmSongname() + "\n" +
-                                            "Album:           " + clickedItem3.getmAlbum() + "\n" +
-                                            "Genre:            " + clickedItem3.getmGenre() + "\n" +
-                                            "Language:     " + clickedItem3.getmLanguage())
-                                    .setPositiveButton("CLOSE", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialogInterface, int i) {
-
-                                        }
-                                    });
-                            builder.show();
-                            return true;
-                    }
-
-                }
-            }
-            return false;
-        }
-
-        @Override
-        public void onClick(View view) {
-            if (mListener != null) {
-                final int position = getAdapterPosition();
-                if (position != RecyclerView.NO_POSITION) {
-                    mListener.onItemClick(position);
-                    final SongsHistoryItem clickedItem = mSongsHistoryList.get(position);
-                }
-            }
-        }
-    }
-
-    public SongsHistoryAdapter(ArrayList<SongsHistoryItem> exampleList) {
-        mSongsHistoryList = exampleList;
-        songsHistoryListFull = new ArrayList<>(exampleList);
-    }
-
-    @NonNull
-    @Override
-    public SongsHistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.songshistoryitem, parent, false);
-        SongsHistoryViewHolder shvh = new SongsHistoryViewHolder(v);
-        return shvh;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull SongsHistoryViewHolder holder, int position) {
-        SongsHistoryItem currentItem = mSongsHistoryList.get(position);
-        holder.mSongname.setText(currentItem.getmSongname());
-        holder.mArtistname.setText(currentItem.getmArtistname());
-    }
-
-    @Override
-    public int getItemCount() {
-        return mSongsHistoryList.size();
     }
 
     @Override
@@ -186,5 +156,4 @@ public class SongsHistoryAdapter extends RecyclerView.Adapter<SongsHistoryAdapte
             notifyDataSetChanged();
         }
     };
-
 }
